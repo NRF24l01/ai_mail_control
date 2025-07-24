517c65bdd349a81e70c55e1563f267021491b517
nrf24l01 <s.nevrovsky@ya.ru>
2025-07-24 12:52:41 +0300

    Fixed default model name

--- models/settings.py
+++ models/settings.py
@@ -4,6 +4,6 @@ from tortoise import fields, models
 class Settings(models.Model):
     id = fields.IntField(pk=True, default=1)
     gpt_prompt = fields.TextField(default="Тебе предложено письмо, твоя задача - определить что это за письмо, варианты: {types}. Если не можешь определить, то ответь 'Undetectable'. Твой ответ долен содержать только одно слово, без кавычек и других символов.")
-    gpt_model = fields.CharField(max_length=255, default="gpt-4o-nano")
+    gpt_model = fields.CharField(max_length=255, default="gpt-4.1-nano")
     answers = fields.JSONField(default={"missmail": "Вам не к нам"})
     types = fields.JSONField(default=["missmail", "spam", "question", "answer", "other"])
